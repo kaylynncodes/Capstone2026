654d5458e63c4c74d758136204042c6da09c6edf
kaylynncodes <180997431+kaylynncodes@users.noreply.github.com>
2026-04-16 17:38:25 -0400

    Create studentsData.js

---
+++ js/studentsData.js
@@ -0,0 +1,63 @@
+export const students = {
+
+  jose: {
+    name: "Jose",
+    location: "Texas",
+    languages: "Spanish, English",
+    story: "I grew up in both Mexico and Texas where school environments were very different. In the U.S., schools were more developed and structured, while in Mexico they were less resourced but more close-knit.",
+    struggle: "I felt strange hearing English at first and struggled with confidence speaking due to my accent.",
+    help: "One-on-one conversations helped me improve the most.",
+    advice: "Don’t stress grammar too much. Practice speaking and it will come naturally."
+  },
+
+  hailey: {
+    name: "Hailey",
+    location: "Texas",
+    languages: "Spanish, English",
+    story: "I grew up in a diverse Texas community and was exposed to both languages early.",
+    struggle: "Sometimes I got confused switching between languages.",
+    help: "Kids shows and songs helped a lot.",
+    advice: "Be patient with yourself,learning takes time."
+  },
+
+  michelle: {
+    name: "Michelle",
+    location: "Texas",
+    languages: "Spanish, English",
+    story: "I started in bilingual programs and later transitioned to English-only classes.",
+    struggle: "I felt intimidated speaking English at first.",
+    help: "Surrounding myself with English speakers helped.",
+    advice: "Don’t give up. It becomes easier over time."
+  },
+
+  nada: {
+    name: "Nada",
+    location: "New Jersey",
+    languages: "Tashelhit, Arabic, English",
+    story: "I grew up between Morocco and Philadelphia and felt like an outsider at first.",
+    struggle: "I was discouraged from speaking my native language.",
+    help: "Culture and music helped me stay connected.",
+    advice: "Don’t worry about sounding ‘foreign’ just express yourself."
+  },
+
+  tracy: {
+    name: "Tracy",
+    location: "Georgia",
+    languages: "Hakha Chin, English",
+    story: "I grew up surrounded by people from my background which made me comfortable.",
+    struggle: "I am starting to lose connection with my native language.",
+    help: "Being around English speakers helped me learn.",
+    advice: "Have fun while learning—it makes a big difference."
+  },
+
+  murk: {
+    name: "Murk",
+    location: "Kazakhstan",
+    languages: "Russian",
+    story: "I am learning English in Kazakhstan but find it difficult sometimes.",
+    struggle: "Learning methods are not always engaging.",
+    help: "Watching English videos helped.",
+    advice: "Make learning more interesting."
+  }
+
+};
